6843d9c9a83861cb1f8fd536262deac20b51b3cb
aaron-cav <89648507+aaron-cav@users.noreply.github.com>
2021-09-15 10:55:14 -0400

    Added java comments
    9/15/2021

--- HolyGrail.java
+++ HolyGrail.java
@@ -1,6 +1,15 @@
-import java.util.Scanner;
+/**
+ * holy grail is an interactive program that ask the user for input and then returns their answer.
+ * @author Aaron Caveney
+ * @version 9/15/2021
+ */
 
-public class HolyGrail {
+import java.util.Scanner;
+public class HolyGrail{
+    /**
+     * Main entry point
+     * @param args none
+     */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("* A chat with the bridge keeper *");
@@ -12,6 +21,8 @@ public class HolyGrail {
         String quest = input.nextLine();
         System.out.print("Question 3: What is your favorite color?");
         String color = input.nextLine();
-        System.out.print("Your name is:" + name +"\n Your quest is:" + quest +"\n Your favorite color is: " + color);
+        System.out.print("King Arthur says, \"You have to know these things when you're a\n" +
+                "king, you know.\"");
+        System.out.print("\nYour name is:" + name + "\n Your quest is:" + quest + "\n Your favorite color is: " + color);
     }
 }
