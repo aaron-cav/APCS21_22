4d54ea53f98e769036a7b5b035a60bffb9e66249
aaron-cav <89648507+aaron-cav@users.noreply.github.com>
2021-11-11 10:23:03 -0500

    11/11/2021
    I fixed the logical error. Added a try catch statement to handled invalid information.

--- BMICalculator.java
+++ BMICalculator.java
@@ -6,13 +6,23 @@ import java.util.Scanner;
  * @Auther Aaron Caveney
  */
 public class BMICalculator {
-
+    /**
+     * This method is doing the formula to BMI
+     * @param inches
+     * @param pounds
+     * @return
+     */
     public static double computeBMI(int inches, int pounds) {
         if(inches <= 0 || pounds < 0)
             return 0;
-        return pounds*.454 / Math.pow(inches*0.254, 2);
+        return pounds*.454 / Math.pow(inches*0.0254, 2);
     }
 
+    /**
+     * This method is finding the how tall the user is in inches
+     * @param heightIN
+     * @return
+     */
     public static int extractInches(String heightIN) {
         int qtPos = heightIN.indexOf("'");
         int dblQtPos = heightIN.indexOf("\"");
@@ -27,24 +37,33 @@ public class BMICalculator {
         return (feet * 12 + inches == 0) ? -1 : feet * 12 + inches;
     }
 
+    /**
+     * this is the main for the program. this method is initalizing and running the program
+     * @param args
+     */
     public static void main(String[] args)  {
-        StringInputParser obj = new StringInputParser();
         Scanner in = new Scanner(System.in);
         DecimalFormat df = new DecimalFormat("0.00");
         while(true) {
-            System.out.print("Enter your height in feet and inches (Ex 6'1\"): ");
-            String height = in.nextLine();
-            int inches = obj.extractInches(height);
-            System.out.print("Enter your weight in pounds: ");
-            Integer weight = in.nextInt();
-            in.nextLine();
-            //System.out.println("your BMI is: "+ df.format(computeBMI(height, weight)));
-            System.out.println(computeBMI(extractInches(height), weight));
-            System.out.println("Your BMI, Expresed as weight(kg)/height(m)^2: ");
-            System.out.println("Continue (Y/N): ");
-            String cont = in.nextLine();
-            if (!cont.toLowerCase().equals("y"))
-                break;
+            try {
+                System.out.print("Enter your height in feet and inches (Ex 6'1\"): ");
+                String height = in.nextLine();
+                //int inches = extractInches(height);
+                System.out.print("Enter your weight in pounds: ");
+                Integer weight = in.nextInt();
+                in.nextLine();
+                //System.out.println("your BMI is: "+ df.format(computeBMI(height, weight)));
+                //System.out.println(computeBMI(extractInches(height), weight));
+                System.out.println("Your BMI, Expresed as weight(kg)/height(m)^2: " + df.format(computeBMI(extractInches(height), weight)));
+                System.out.print("Continue (Y/N): ");
+                String cont = in.nextLine();
+                if (!cont.toLowerCase().equals("y"))
+                    break;
+            }
+            catch(Exception e)  {
+                System.out.println("Invalid data entered, please try again.");
+                in.nextLine();
+            }
         }
         System.out.println("end of program");
     }
